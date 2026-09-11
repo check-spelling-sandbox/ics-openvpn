d439ab5c0027b1b32b132fc083b0135b15cac667
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-09-11 10:00:25 -0400

    spelling: in case,
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- main/src/main/java/de/blinkt/openvpn/core/OpenVPNService.java
+++ main/src/main/java/de/blinkt/openvpn/core/OpenVPNService.java
@@ -821,7 +821,7 @@ public class OpenVPNService extends VpnService implements StateListener, Callbac
             mDeviceStateReceiver = null;
         }
         mCommandHandlerThread.quit();
-        // Just in case unregister for state
+        // Just in case, unregister for state
         VpnStatus.removeStateListener(this);
         VpnStatus.flushLog();
     }
